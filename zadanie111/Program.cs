86d205bdd0c55b8009b2d022427047b63a3ea3ce
bethixx <lisa39942@gmail.com>
2025-04-15 16:24:39 +0200

    Prosty kalkulator

--- zadanie111/Program.cs
+++ zadanie111/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prosty Kalkulator");
+            Console.WriteLine("-------------------");
+
+            Console.Write("Podaj pierwszą liczbę: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Podaj drugą liczbę: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Podaj operator (+, -, *, /): ");
+            char operation = Console.ReadLine()[0];
+
+            double result;
+
+            switch (operation)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    if (secondNumber != 0)
+                    {
+                        result = firstNumber / secondNumber;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie można dzielić przez zero!");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Nieprawidłowy operator!");
+                    return;
+            }
+
+            Console.WriteLine($"Wynik: {result}");
+        }
+    }
+}
--- zadanie111/obj/Debug/net8.0/zadanie111.AssemblyInfo.cs
+++ zadanie111/obj/Debug/net8.0/zadanie111.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zadanie111")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa25becd12a47f1aa6266f63d7d52053a2d0a1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("zadanie111")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zadanie111")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
